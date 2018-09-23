a539816518a0af6eae4eef719bab2745c432a492
Ray X <531482780@qq.com>
2018-09-23 22:07:40 +0800

    添加切换城市和手动更新天气的功能

--- app/src/main/java/com/silenceweather/fragment/ChooseAreaFragment.java
+++ app/src/main/java/com/silenceweather/fragment/ChooseAreaFragment.java
@@ -17,6 +17,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.silenceweather.HomeActivity;
 import com.silenceweather.R;
 import com.silenceweather.WeatherActivity;
 import com.silenceweather.db.City;
@@ -97,10 +98,18 @@ public class ChooseAreaFragment extends Fragment {
                 }
                 if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof HomeActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
+
                 }
             }
         });
